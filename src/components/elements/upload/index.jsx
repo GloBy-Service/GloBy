5827ec5ec337400c628388616c2722f6ee59911c
kenanmusali <textme0306@gmail.com>
2025-07-12 01:15:16 +0400

    Testing ...

--- src/components/elements/upload/index.jsx
+++ src/components/elements/upload/index.jsx
@@ -151,7 +151,10 @@ const Upload = ({ country, stayDays }) => {
       const errorPhrases = [
         'Ortalama gündəlik kredit ölkə üçün tələb olunan minimumdan azdır',
         'Average daily expense is below the required minimum',
-        'Average daily expense for asked country is less than minimum'
+        'Average daily expense for asked country is less than minimum',
+        'Period is too big.. Maximum accepted period is : 185 days',
+        'Minimum required days are not meet. Minimum: 28 days required',
+        'Rules not found.'
       ];
 
       const isInsufficientFunds = errorPhrases.some(phrase =>
